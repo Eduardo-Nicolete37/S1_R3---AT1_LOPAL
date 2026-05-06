6e0dc0c6ee2e1fc87e3d3758c76de2970d230d52
Eduardo Nicolete <eduardonicolete37@gmail.com>
2026-05-06 14:39:13 -0300

    Fix duplicate print statement for even numbers

--- 1.py
+++ 1.py
@@ -5,8 +5,8 @@ print("Você quer ver os números pares ou ímpares?")
 option = input("(Ímpar - I)/(Par - P): ")
 
 if option == "P" or option == "p":
+    print("Números pares: ")
     for i in range(0, 101, 2):
-        print("Números pares: ")
         print(i)
 elif option == "I" or option == "i":
     print("Números ímpares: ")
