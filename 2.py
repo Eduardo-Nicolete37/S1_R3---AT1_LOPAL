9e359535206a74dff907ee8de4e370bfaf2dbbd3
Eduardo Nicolete <eduardonicolete37@gmail.com>
2026-05-06 15:10:22 -0300

    Improvement of the code

--- 2.py
+++ 2.py
@@ -5,12 +5,8 @@ first = float(input("Por favor, nos diga o 1° digito: "))
 second = float(input("Agora, nos diga o 2° digito: "))
 third = float(input("E por último, nos diga o 3° digito: "))
 
-if first > third:
-    first, third = third, first
-elif first > second:
-    first, second = second, first
-elif second > third:
-    second, third = third, second
+lista = [first, second, third]
 
-print(f"O maior dos três é: {third}")
-print(f"Já o menor é: {first}")
+
+print(f"O maior dos três é: {lista[2]}")
+print(f"Já o menor é: {lista[0]}")
