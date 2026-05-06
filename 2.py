b3f30130dfda4a06cbc36f3ec5dabe0cca612fdd
Eduardo Nicolete <eduardonicolete37@gmail.com>
2026-05-06 15:11:52 -0300

    Improvement of the code inside 2.py

--- 2.py
+++ 2.py
@@ -6,7 +6,7 @@ second = float(input("Agora, nos diga o 2° digito: "))
 third = float(input("E por último, nos diga o 3° digito: "))
 
 lista = [first, second, third]
-
+lista.sort()
 
 print(f"O maior dos três é: {lista[2]}")
 print(f"Já o menor é: {lista[0]}")
